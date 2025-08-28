e6ad41fe76943853a1c1e62fd4f02b47aff285b8
Tungstend <hanjinlong197@gmail.com>
2025-08-28 19:36:06 +0800

    1.2

--- src/main/java/org/fcl/enchantnetcore/LibraryInfo.java
+++ src/main/java/org/fcl/enchantnetcore/LibraryInfo.java
@@ -4,8 +4,8 @@ public class LibraryInfo {
 
     public static final String DEVELOPERS     = "Tungsten, ShirosakiMio";
 
-    public static final String CORE_VERSION   = "1.1";
+    public static final String CORE_VERSION   = "1.2";
 
-    public static final int CORE_VERSION_CODE = 110;
+    public static final int CORE_VERSION_CODE = 120;
 
 }
